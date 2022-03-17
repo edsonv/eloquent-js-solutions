b2c2e6f60f840703c886a9644992544de1701967
Edson J Vargas <edson.jhair@yahoo.com>
2022-03-17 18:36:44 -0500

    ADD: Chapter 5 exercises solutions helpers

---
+++ chapter5/helpers/characterScripts.js
@@ -0,0 +1,12 @@
+exports.characterScript = code => {
+  for (let script of SCRIPTS) {
+    if (
+      script.ranges.some(([from, to]) => {
+        return code >= from && code < to;
+      })
+    ) {
+      return script;
+    }
+  }
+  return null;
+};
